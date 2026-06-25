362aec7659521919448b6fdb8c70f0914aca9de6
Rajveer singh <raj07092004@gmail.com>
2026-06-25 15:01:13 +0530

    fuuuu

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello, React!</h1>
-      <p>Welcome to my React application.</p>
+    <div className="App">
+      <h1>Welcome to React</h1>
+      <p>Rajveer Singh</p>
     </div>
   )
 }
---
+++ src/Components/Cards.jsx
@@ -0,0 +1,14 @@
+function card()
+{
+    return (
+        <div className="card">
+            <img src="https://via.placeholder.com/150" alt="Card Image" />
+            <h2>Card Title</h2>
+            <p>This is a description of the card. It provides more details about the content.</p>
+            <button>Learn More</button>
+        </div>
+    );
+
+}
+
+export default card;
